39b18ab633de363a4724764488cf3448b76af81f
Tomasz H <tom@nece.dev>
2022-06-07 06:41:17 +0100

    Create App.svelte

---
+++ App.svelte
@@ -0,0 +1,75 @@
+<script>
+
+</script>
+
+
+
+<main>
+<nav class="navbar is-fixed-top is-light has-shadow">
+<!--Logo-->
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png"
+        width="112" height="28" alt="logo">
+      </a>
+      <div class="navbar-burger" id="burger">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+    
+    
+    </div>
+
+    <div class="navbar-menu" id="menu">
+      <div class="navbar-start">
+        <a href="?" class="navbar-item">
+          Home
+        </a>
+        <a href="?" class="navbar-item">
+          Documentation
+        </a>
+        <a href="?" class="navbar-item">
+          About
+        </a>
+        <a href="?" class="navbar-item">
+          Jobs
+        </a>
+        <a href="?" class="navbar-item">
+          Contact
+        </a>
+        <a href="?" class="navbar-item">
+          Report an issue
+        </a>
+      </div>
+    </div>
+
+</nav>
+
+  <br/>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    Fugit recusandae facilis aliquam laboriosam laudantium.
+
+</main>
+
+<style lang="scss">
+
+	$primary: purple;
+
+
+	@import 'bulma/bulma';
+
+  #burger {
+
+    cursor:default;
+    &:hover {
+      background-color: unset !important;
+      color: unset !important;
+    }
+  }
+
+
+
+  
+</style>
